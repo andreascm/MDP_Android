e29486430cfe24983c34c4c53c0930be5b6b506e
qwerasdzx2 <qwerasdzx2@hotmail.com>
2016-09-26 10:30:50 +0800

    26/9 1030

--- app/src/main/java/mdp/mdp_android/ReconfigureButtons.java
+++ app/src/main/java/mdp/mdp_android/ReconfigureButtons.java
@@ -1,9 +1,15 @@
 package mdp.mdp_android;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
 
 public class ReconfigureButtons extends ActionBarActivity {
+
+    private SharedPreferences mSharedPreferences;
+    private EditText edittextF1Config, edittextF2Config;
+    private String F1Config, F2Config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
