134331bf40c9a7beaa097c91b1d9f2074e260ac8
qwerasdzx2 <qwerasdzx2@hotmail.com>
2016-09-27 10:24:00 +0800

    27/9 1023

--- app/src/main/java/mdp/mdp_android/BluetoothTesting.java
+++ app/src/main/java/mdp/mdp_android/BluetoothTesting.java
@@ -1,14 +1,46 @@
 package mdp.mdp_android;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class BluetoothTesting extends ActionBarActivity {
+
+    private SharedPreferences mSharedPreferences;
+    private EditText sendmessage, receivedmessage;
+    private Button sendmessagebutton;
+    private Bluetooth bluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_testing);
-    }
 
+        receivedmessage= (EditText) findViewById(R.id.receivedmessage) ;
+        sendmessage = (EditText) findViewById(R.id.sendmessage);
+        bluetooth = MainActivity.getBluetooth();
+        
+        sendmessagebutton = (Button) findViewById(R.id.sendmessagebutton);
+        mSharedPreferences = getSharedPreferences("UserConfiguration",
+                MODE_PRIVATE);
+
+        receivedmessage.setText(mSharedPreferences.getString("receivedmessage",""));
+        sendmessage.setText(mSharedPreferences.getString("sendmessage", ""));
+
+        sendmessagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String message = sendmessage.getText().toString();
+
+                if (bluetooth != null) {
+                    bluetooth.write(message.getBytes());
+                }
+                finish();
+            }
+        });
+    }
 }
